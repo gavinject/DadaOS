b83cdae7c3af978c019bbdef3161b680eb2d8478
gavinject <ggav7n@gmail.com>
2026-03-23 13:37:53 -0500

    Create DadaOS-TamperMonkey.js with TamperMonkey script header and DadaOS code

---
+++ DadaOS-TamperMonkey.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         DadaOS
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  A script for DadaOS
+// @author       gavinject
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    // Your DadaOS code goes here
+    console.log('DadaOS script is running');
+})();
